db64a8645bb10a0f2d17ed423185784517180b2c
Repo manager <rugiguru@gmail.com>
2020-03-03 18:18:35 +0530

    added

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const Joi = require('@hapi/joi');
 const log = require('./middlewares/logger');
 // const auth = require('./middlewares/Authenticate')
 const morgan = require('morgan')
-app.use(express.json())
 const genres = require('./routes/genres');
-const customers = require('./routes/customers')
+const customers = require('./routes/customers');
 const movies = require('./routes/movies');
-const rentals = require("./routes/rentals")
-const users = require("./routes/users")
+const rentals = require("./routes/rentals");
+const users = require("./routes/users");
 const auth = require("./routes/auth");
+
+app.use(express.json());
 app.use('/api/geners', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
@@ -31,3 +32,4 @@ app.listen(3000, () => console.log('The server is running at 3000...'));
 mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true,useUnifiedTopology: true })
     .then((res) => console.log('Connected to MongoDB...'))
     .catch((err) => console.error('Could not connect to MongoDB...', err))
+
